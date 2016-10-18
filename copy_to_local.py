4a21d628cae5008877e8b59bb0c5d9a221ab2f06
Keroles Hakem <keroleshakem@gmail.com>
2016-10-18 15:41:17 -0400

    -fix copy to file issue
    - fix copy to file issue and now file should works the same way

--- copy_to_local.py
+++ copy_to_local.py
@@ -1,12 +1,11 @@
 import urllib2  # the lib that handles the url stuff
 from sys import argv
 
-script, filename = argv
-target_url = raw_input("> ")
+target_url = argv
+
 #target_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wpbc.data";
 j = open("local.txt",'w')
-
-data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+data = urllib2.urlopen(argv[1]) # it's a file like object and works just like a file
 for line in data: # files are iterable
     j.write(line)
 
